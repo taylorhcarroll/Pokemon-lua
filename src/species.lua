5215a935f4d9933033fde151b2ebd9e6a12eeb0d
Taylor Harrison Carroll <taylorhcarroll1@gmail.com>
2022-06-18 15:03:35 -0400

    update

--- src/species.lua
+++ src/species.lua
@@ -2,7 +2,12 @@ Species = Class{}
 
 function Species:init(def, level)
     self.ID = def.ID
+    self.speciesName = def.speciesName
     self.battleSpriteFront = def.battleSpriteFront
     self.battleSpriteBack = def.battleSpriteBack
-    
+    self.PrimaryType = def.PrimaryType
+    self.SecondaryType = def.SecondaryType
+    self.height = def.height
+    self.weight = def.weight
+
 end
