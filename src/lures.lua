5d5351b38e9d8fb10fa3894923f0d16a8197f543
Taylor Harrison Carroll <taylorhcarroll1@gmail.com>
2022-06-18 15:32:09 -0400

    added link to crankinput

--- src/lures.lua
+++ src/lures.lua
@@ -3,4 +3,6 @@ Lures = Class{}
 --this is the replacement for poke balls
 function Lures:init(def, type)
 
+--reference here for crank input
+--https://sdk.play.date/1.12.0/Inside%20Playdate.html#_reading_crank_input
 end
