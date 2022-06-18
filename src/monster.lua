faa7f73f49d0ad042af50e9a5ff7346837368ce3
Taylor Harrison Carroll <taylorhcarroll1@gmail.com>
2022-06-18 14:56:11 -0400

    initial pokemon and species class file

---
+++ src/monster.lua
@@ -0,0 +1,22 @@
+Monster = Class{}
+
+--add species as input?
+function Monster:init(def, level)
+    self.ID = def.ID
+    self.name = def.name
+
+    -- do I need this here?
+    self.currentHP = def.currentHP
+
+    self.baseHP = def.baseHP
+    self.speed = def.speed
+    self.defense = def.defense
+    self.attack = def.attack
+    self.specAttack = def.specAttack
+    self.specDefense = def.specDefense
+
+    self.level = def.level
+    self.gender = def.gender
+    self.speciesID = def.spec
+
+end
---
+++ src/species.lua
@@ -0,0 +1,7 @@
+Species = Class{}
+
+function Species:init(def, level)
+    self.ID = def.ID
+    self.battleSpriteFront = def.battleSpriteFront
+    self.battleSpriteBack = def.battleSpriteBack
+end
