ac2786f1946ff2acab8a40ec4395d9f35500f82c
Taylor Harrison Carroll <taylorhcarroll1@gmail.com>
2022-06-18 15:57:47 -0400

    .

--- src/lures.lua
+++ src/lures.lua
@@ -5,4 +5,5 @@ function Lures:init(def, type)
 
 --reference here for crank input
 --https://sdk.play.date/1.12.0/Inside%20Playdate.html#_reading_crank_input
+--https://www.youtube.com/watch?v=NoziaNaBhLo
 end
